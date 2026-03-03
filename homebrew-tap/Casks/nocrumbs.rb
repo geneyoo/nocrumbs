10e6e840580cfaff9decc1771752ab81a0f61c05
Gene <geneyoois@gmail.com>
2026-03-03 11:15:43 -0800

    fix: set correct SHA256 for v0.5.5 cask

--- homebrew-tap/Casks/nocrumbs.rb
+++ homebrew-tap/Casks/nocrumbs.rb
@@ -1,6 +1,6 @@
 cask "nocrumbs" do
   version "0.5.5"
-  sha256 "PLACEHOLDER"
+  sha256 "ffbdb5f5ca96aaac08b97691af1379955562e05abb379550e290df7431541793"
 
   url "https://github.com/geneyoo/nocrumbs/releases/download/v#{version}/NoCrumbs-#{version}.zip"
   name "NoCrumbs"
